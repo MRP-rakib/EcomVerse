5fda9d1ea7dc0eed81f9a671fe1c378496e2bfe0
MRP-rakib <rakib260.hossain@gmail.com>
2025-04-15 02:58:16 +0600

    loading text resize

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SkeletonCard() {
   return (
     <div className=" animate-pulse p-2 rounded-lg shadow-sm bg-gray-100">
-    <p className="h-75 bg-gray-200 rounded flex items-center justify-center text-[30px] font-bold">Loading...</p>
+    <p className="h-75 bg-gray-200 rounded flex items-center justify-center text-[20px] font-bold">Loading...</p>
    </div>
   )
 }
