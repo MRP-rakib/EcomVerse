6b061ca5578be4378554bb6e4c699913af465d38
MRP-rakib <rakib260.hossain@gmail.com>
2025-04-15 17:42:56 +0600

    section heading add

--- src/components/NewArrival.jsx
+++ src/components/NewArrival.jsx
@@ -3,24 +3,28 @@ import Ps5 from "../assets/images/console.png"
 import Cap from "../assets/images/cap.png"
 import frame from "../assets/images/Frame707.png"
 import perfume from "../assets/images/perfume.png"
-
+import ProductSection from './ProductSection'
 function NewArrival() {
     return (
-        <div className='pt-12 lg:pt-[168px] grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-7.5 '>
-             <NewArrivalProduct1 productName="PlayStation 5" caption="Black and White version of the PS5 coming out on sale."  img={Ps5} className={"pt-[90px] justify-center "}/>
-            <div className='grid grid-cols-1 gap-4 lg:gap-7.5'>
-                <div>
-                <NewArrivalProduct1  productName="Women’s Collections" caption="Featured woman collections that give you another vibe."  img={Cap} className={"justify-end"}/>
-                </div>
-                <div className='grid grid-cols-2 gap-4 lg:gap-7.5 '>
-                    <NewArrivalProduct1  productName="Speakers" caption="Amazon wireless speakers" img={frame} className={"justify-center py-8"} />
-                    <NewArrivalProduct1  productName="Perfume" caption="GUCCI INTENSE OUD EDP"  img={perfume} className={"justify-center py-8"}/>
+        <div className='pt-12 lg:pt-[168px] '>
+            <ProductSection sectionName="New Arrival" sectionType='Featured' />
+            <div className='pt-7.5 lg:pt-15 grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-7.5 '>
+                <NewArrivalProduct1 productName="PlayStation 5" caption="Black and White version of the PS5 coming out on sale." img={Ps5} className={"pt-[90px] justify-center "} />
+                <div className='grid grid-cols-1 gap-4 lg:gap-7.5'>
+                    <div>
+                        <NewArrivalProduct1 productName="Women’s Collections" caption="Featured woman collections that give you another vibe." img={Cap} className={"justify-end"} />
+                    </div>
+                    <div className='grid grid-cols-2 gap-4 lg:gap-7.5 '>
+                        <NewArrivalProduct1 productName="Speakers" caption="Amazon wireless speakers" img={frame} className={"justify-center py-8"} />
+                        <NewArrivalProduct1 productName="Perfume" caption="GUCCI INTENSE OUD EDP" img={perfume} className={"justify-center py-8"} />
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
-function NewArrivalProduct1({img,className,productName,caption}) {
+function NewArrivalProduct1({ img, className, productName, caption }) {
     return (
         <div className='bg-black relative cursor-pointer'>
             <div className={`flex items-end ${className}`}>
